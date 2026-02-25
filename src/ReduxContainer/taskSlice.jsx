8a5fa588f6b1e507356b9f26ca57a61eb745051d
Mahmoud Habib <mahmoudhabeb96@gmail.com>
2026-02-25 23:20:50 +0200

    react-redux&ReduxContainer for the app configured

---
+++ src/ReduxContainer/taskSlice.jsx
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+/* fetchTasks via Redux AsyncThunk */
+export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
+    const res = await fetch("http://localhost:4000/tasks");
+    const data = await res.json();
+    return data;
+});
+
+/* create redux container slice */
+const taskReducer = createSlice({
+    name: "tasks",
+    initialState: {
+        loading: false,
+        tasks: [],
+        error: ''
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchTasks.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchTasks.fulfilled, (state, action) => {
+            state.loading = false;
+            state.users = action.payload
+        });
+        builder.addCase(fetchTasks.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message
+        });
+    }
+});
+
+export default taskReducer.reducer;
